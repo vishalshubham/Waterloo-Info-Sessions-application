a658f9cb2f2fe12dc6ea6be11aaedd8de79abe43
vishalshubham <vishal.shubham@gmail.com>
2015-08-10 23:31:36 -0400

    changes

--- app/src/main/java/uwinfosessions/uwinfosessions/activity/InfoSessionListActivity.java
+++ app/src/main/java/uwinfosessions/uwinfosessions/activity/InfoSessionListActivity.java
@@ -6,11 +6,17 @@ import android.content.res.TypedArray;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import uwinfosessions.uwinfosessions.R;
 import uwinfosessions.uwinfosessions.adapter.TabsPagerAdapter;
@@ -24,16 +30,47 @@ public class InfoSessionListActivity extends FragmentActivity implements ActionB
     public ActionBar actionBar;
     public float actionBarHeight;
 
+    DrawerLayout drawerLayout;
+    ListView drawerListView;
+    ActionBarDrawerToggle drawerToggle;
+    String title="";
+
     private String[] tabs = { "All", "Today", "Favourite", "Reminder" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
-        final TypedArray styledAttributes = getTheme().obtainStyledAttributes(new int[] { android.R.attr.actionBarSize });
-        actionBarHeight = styledAttributes.getDimension(0, 0);
-        styledAttributes.recycle();*/
         setContentView(R.layout.activity_info_session_list);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerListView = (ListView)findViewById(R.id.drawer_list);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, null, R.string.drawer_open, R.string.drawer_close){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                getActionBar().setTitle("Info Sessions");
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle("Menu");
+            }
+        };
+
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(), R.layout.activity_info_session_list_item_drawer, getResources().getStringArray(R.array.string_menu_options));
+        drawerListView.setAdapter(adapter);
+        getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String[] menu_options = getResources().getStringArray(R.array.string_menu_options);
+
+            }
+        });
 
         viewPager = (ViewPager)findViewById(R.id.pager);
         tabAdapter = new TabsPagerAdapter(getSupportFragmentManager());
@@ -67,6 +104,12 @@ public class InfoSessionListActivity extends FragmentActivity implements ActionB
     }
 
     @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_info_session_list, menu);
@@ -75,17 +118,18 @@ public class InfoSessionListActivity extends FragmentActivity implements ActionB
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerListView);
+        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
